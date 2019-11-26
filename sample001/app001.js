a9616b2130c21edd0a5af8f9c072ac739e18ce47
y.tadano <tadano.wk.git@gmail.com>
2019-11-27 01:15:24 +0900

    デバッグまで構築

---
+++ sample001/app001.js
@@ -0,0 +1,10 @@
+"use strict";
+var messege1 = "HELLO1";
+console.info(messege1);
+var messege2 = "HELLO2";
+console.info(messege2);
+var messege3 = "HELLO3";
+console.info(messege3);
+var messege4 = "HELLO4";
+console.info(messege4);
+//# sourceMappingURL=app001.js.map
---
+++ sample001/app001.ts
@@ -0,0 +1,8 @@
+let messege1 = "HELLO1";
+console.info(messege1);
+let messege2 = "HELLO2";
+console.info(messege2);
+let messege3 = "HELLO3";
+console.info(messege3);
+let messege4 = "HELLO4";
+console.info(messege4);
